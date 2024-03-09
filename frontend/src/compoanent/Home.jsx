4045b22bc6d58014b942c6fe98fe04f9efad10f6
rachidcooder <cooderherou@gmail.com>
2024-03-09 17:00:26 +0000

    navBar update

--- frontend/src/compoanent/Home.jsx
+++ frontend/src/compoanent/Home.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='max-x-[1640px] mx-auto p-4'>
-       <div className='max-h-[500px] relative'>
+    <div className='max-x-[1640px] mx-auto'>
+      <div className='max-h-[500px] relative'>
         <div className='h-full w-full absolute justify-center flex flex-col max-h-[500px] bg-black/50 px-4'>
           <h1 className='text-4xl font-bold text-white lg:text-7xl sm:text-5xl'> The <span className=' text-orange-600'> Best</span></h1>
           <h1 className=' text-orange-600 text-4xl font-bold lg:text-7xl sm:text-5xl'> Food <span className='text-white'> Delivary</span></h1>
         </div>
-        <img   className='w-full max-h-[500px] object-cover'
-            src='https://images.pexels.com/photos/2983099/pexels-photo-2983099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/'/>
-      
-       </div>
+        <img className='w-full max-h-[500px] object-cover'
+          src='https://images.pexels.com/photos/2983099/pexels-photo-2983099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='/' />
+
+      </div>
     </div>
   )
 }
--- frontend/src/compoanent/NavBar.jsx
+++ frontend/src/compoanent/NavBar.jsx
@@ -1,61 +1,62 @@
 import React, { useState } from 'react'
-import {AiOutlineMenu ,AiOutlineSearch ,AiOutlineClose,AiFillTag} from 'react-icons/ai'
-import {BsFillCartFill } from "react-icons/bs"
-import {TbTruckDelivery} from "react-icons/tb"
-import{MdFavorite ,MdHelp} from "react-icons/md"
-import {FaWallet ,FaUserFriends } from "react-icons/fa"
-import { BsFillSaveFill} from "react-icons/bs"
+import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai'
+import { BsFillCartFill } from "react-icons/bs"
+import { TbTruckDelivery } from "react-icons/tb"
+import { MdFavorite, MdHelp } from "react-icons/md"
+import { FaWallet, FaUserFriends } from "react-icons/fa"
+import { BsFillSaveFill } from "react-icons/bs"
+import { IoSearchSharp } from "react-icons/io5";
 
 function NavBar() {
-  const[nav,setNav]=useState(false) ;
+  const [nav, setNav] = useState(false);
 
   return (
     <div>
-      <div className='flex items-center justify-between py-2'>
+      <div className='flex items-center justify-between w-full z-50 fixed top-0 
+         h-[60px] text-gray-50 bg-orange-700 '>
         <div className='flex items-center'>
-            <AiOutlineMenu size={30} className=''  onClick={()=>{setNav(true)}}/>
+          <AiOutlineMenu size={30} className='' onClick={() => { setNav(true) }} />
           <h1 className=' lg:text-2xl mx-2 font-bold'>Onlay Food</h1>
-          <div className=' bg-gray-100 rounded-lg flex items-center px-4 py-1 hidden lg:flex '>
-             <p className=' rounded-s-full bg-black text-white px-2'>Delivery</p>
-             <p className='px-1'>Pickup</p>
+          <div className='  rounded-lg flex items-center px-4 py-1  lg:flex '>
+            <p className=' rounded-s-full bg-white text-orange-700 px-2'>Delivery</p>
+            <p className='px-1'>Pickup</p>
           </div>
-         
+
         </div>
         <div className='rounded-lg bg-gray-100 flex items-center'>
-          <AiOutlineSearch size={25} className='mx-1'/> 
+          <IoSearchSharp size={25} className='mx-1 text-gray-500' />
           <input type='text' placeholder='search for food'
-                 className='bg-transparent focus:outline-none p-2'/>
+            className='bg-transparent focus:outline-none p-1 text-xl px-2 text-gray-800' />
         </div>
-        <BsFillCartFill size={25} className='mx-4'/>
+        <BsFillCartFill size={25} className='mx-4' />
       </div>
-      { nav ?    <div  className='fixed h-screen w-full z-10 bg-black/80 top-O left-0'>
-      </div>:""
+      {nav ? <div className='fixed h-screen w-full z-10 bg-black/80 top-O left-0'>
+      </div> : ""
 
       }
-  
-{
-  nav ?
-    <div className='h-screen w-[250px] bg-white fixed top-0 left-0 z-10 duration-300'>
-        <div className='flex justify-between '>
-         <h1 className='text-2xl p-4'>Onlay
-       <span className=' font-bold'>Food</span>
-         </h1>
-           <AiOutlineClose size={25} className='my-4 mx-1' onClick={()=>{setNav(false)}}/>
-         </div>
-        <ul className=' flex flex-col p-1 text-gray-800'>
-            <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><TbTruckDelivery size={30} className='mx-1'/> Delivery</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><MdFavorite size={30} className='mx-1'/> Favorites</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><FaWallet size={30} className='mx-1'/> Walet</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><MdHelp size={30} className='mx-1'/> Help</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><AiFillTag size={30} className='mx-1'/> Promotions</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><BsFillSaveFill size={30} className='mx-1'/> Best One</li>
-       <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><FaUserFriends size={30} className='mx-1'/> Invite Friends</li>
-    
-        </ul>
-      </div>
-   :""
-}
-    
+      {
+        nav ?
+          <div className='h-screen w-[250px] bg-white fixed top-0 left-0 z-50 duration-300'>
+            <div className='flex justify-between items-center bg-orange-600 h-[60px] px-3'>
+              <h1 className='text-2xl  text-white'>Onlay
+                <span className=' font-bold'>Food</span>
+              </h1>
+              <AiOutlineClose size={25} className='my-4 mx-1 text-orange-50' onClick={() => { setNav(false) }} />
+            </div>
+            <ul className=' flex flex-col p-1 text-gray-800'>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><TbTruckDelivery size={30} className='me-2 text-orange-600' /> Delivery</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><MdFavorite size={30} className='me-2 text-orange-600' /> Favorites</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><FaWallet size={30} className='me-2 text-orange-600' /> Walet</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><MdHelp size={30} className='me-2 text-orange-600' /> Help</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><AiFillTag size={30} className='me-2 text-orange-600' /> Promotions</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><BsFillSaveFill size={30} className='me-2 text-orange-600' /> Best One</li>
+              <li className=' text-xl py-4 flex hover:bg-gray-100 rounded-lg'><FaUserFriends size={30} className='me-2 text-orange-600' /> Invite Friends</li>
+
+            </ul>
+          </div>
+          : ""
+      }
+
     </div>
   )
 }
